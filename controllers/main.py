3e432ef91bee1229bb203dff8760292b9afb1d1c
gonxi <gonxi.gonzalo@gmail.com>
2015-10-24 03:45:17 +0200

    se isntala y fuciona falta rederigir pago'

--- controllers/main.py
+++ controllers/main.py
@@ -9,14 +9,17 @@ from openerp.http import request
 _logger = logging.getLogger(__name__)
 
 
-class PaymentBicoin(http.Controller):
-	_accept_url = '/payment/transfer/feedback'
+class BitcoinController(http.Controller):
+	
+	_accept_url = '/payment/bitcoin/feedback'
+
 
 	@http.route([
-		'/payment/transfer/feedback',
-	], type='http', auth='none')
-	def transfer_form_feedback(self, **post):
-		cr, uid, context = request.cr, SUPERUSER_ID, request.context
-		_logger.info('Beginning form_feedback with post data %s', pprint.pformat(post))  # debug
-		request.registry['payment.transaction'].form_feedback(cr, uid, post, 'bitcoin', context)
-		return werkzeug.utils.redirect(post.pop('return_url', '/'))
+	 	'/payment/bitcoin/feedback',
+	 	], type='http', auth='none')
+
+	def bitcoin_form_feedback(self, **post):
+	 	cr, uid, context = request.cr, SUPERUSER_ID, request.context
+	 	_logger.info('Beginning form_feedback with post data %s', pprint.pformat(post))  # debug
+	 	request.registry['payment.transaction'].form_feedback(cr, uid, post, 'bitcoin', context)
+	 	return werkzeug.utils.redirect(post.pop('return_url', '/'))
--- models/bitcoin.py
+++ models/bitcoin.py
@@ -10,13 +10,28 @@ class AcquirerPaymentBitcoin(models.Model):
 
 	_inherit = 'payment.acquirer'
 
-	bitcoin_rpc_server = fields.Char('Bitcoin Server rpc')
-	bitcoin_rpc_port = fields.Char('Port server rpc')
-	bitcoin_rpc_username = fields.Char('User rpc')
-	bitcoin_rpc_password = fields.Char('Passwords rpc')
+	bitcoin_address = fields.Char('Bitcoin address')
+	#TODO: Enlazar con servidor rpc
+	# bitcoin_rpc_server = fields.Char('Bitcoin Server rpc')
+	# bitcoin_rpc_port = fields.Char('Port server rpc')
+	# bitcoin_rpc_username = fields.Char('User rpc')
+	# bitcoin_rpc_password = fields.Char('Passwords rpc')
 
 	@api.model
 	def _get_providers(self):
 		providers = super(AcquirerPaymentBitcoin, self)._get_providers()
 		providers.append(['bitcoin', 'Bitcoin'])
 		return providers
+	def transfer_get_form_action_url(self, cr, uid, id, context=None):
+		return '/payment/transfer/feedback'
+
+
+class BitcoinPaymentTransaction(models.Model):
+ 	_inherit = 'payment.transaction'
+
+	def _bitcoin_form_get_tx_from_data(self, cr, uid, data, context=None):
+		reference, amount, currency_name = data.get('reference'), data.get('amount'), data.get('currency_name')
+		tx_ids = self.search(
+		cr, uid, [
+			('reference', '=', reference),
+		], context=context)
