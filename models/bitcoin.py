24104cec6b419d8efd53ca5ee01c40018eb57754
gonxi <gonxi.gonzalo@gmail.com>
2015-10-24 04:10:13 +0200

    Error no existe- global, dejo solo self)

--- models/bitcoin.py
+++ models/bitcoin.py
@@ -20,18 +20,52 @@ class AcquirerPaymentBitcoin(models.Model):
 	@api.model
 	def _get_providers(self):
 		providers = super(AcquirerPaymentBitcoin, self)._get_providers()
-		providers.append(['bitcoin', 'Bitcoin'])
+		providers.append(['bitcoin', _('Bitcoin')])
 		return providers
+
 	def transfer_get_form_action_url(self, cr, uid, id, context=None):
-		return '/payment/transfer/feedback'
+		return '/payment/bitcoin/feedback'
+
+	def _format_bitcoin_data(self, cr, uid, context=None):
+		post_msg = '''<div>
+		<h3>Please use the following transfer details</h3>
+		<h4>Ya queda poco</h4>
+		<h4>Communication</h4>
+		<p>Please use the order name as communication reference.</p>
+		</div>'''
+		return post_msg
+
+	def create(self, cr, uid, values, context=None):
+		""" Hook in create to create a default post_msg. This is done in create
+		to have access to the name and other creation values. If no post_msg
+		or a void post_msg is given at creation, generate a default one. """
+		if values.get('provider') == 'bitcoin' and not values.get('post_msg'):
+			values['post_msg'] = self._format_bitcoin_data(cr, uid, context=context)
+		return super(AcquirerPaymentBitcoin, self).create(cr, uid, values, context=context)
 
 
 class BitcoinPaymentTransaction(models.Model):
+
  	_inherit = 'payment.transaction'
 
-	def _bitcoin_form_get_tx_from_data(self, cr, uid, data, context=None):
+	def _bitcoin_form_get_tx_from_data(self,data,):
 		reference, amount, currency_name = data.get('reference'), data.get('amount'), data.get('currency_name')
 		tx_ids = self.search(
 		cr, uid, [
 			('reference', '=', reference),
 		], context=context)
+
+		if not tx_ids or len(tx_ids) > 1:
+			error_msg = 'received data for reference %s' % (pprint.pformat(reference))
+			if not tx_ids:
+				error_msg += '; no order found'
+			else:
+				error_msg += '; multiple order found'
+			_logger.error(error_msg)
+			raise ValidationError(error_msg)
+
+		return self.browse(cr, uid, tx_ids[0], context=context)
+
+	def _bitcoin_form_validate(self, cr, uid, tx, data, context=None):
+		_logger.info('Validated bitcoin payment for tx %s: set as pending' % (tx.reference))
+		return tx.write({'state': 'pending'})
